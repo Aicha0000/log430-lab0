96469bcceb342ecd7cf99d8275bf830396430f07
Aicha Aanounou <aicha.aanounou1@ens.etsmtl.ca>
2025-05-15 02:57:55 +0000

    added second test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
+import random
+
+def get_random_message():
+    message= ["Keep it up :)", "You got this :)", "You're almost at the finish line :)", "It will be okay :)"]
+    return random.choice(message)
 
 def main():
     print("Hey you!")
+    print(get_random_message())
 
 if __name__ == "__main__":
     main()
--- tests/test_unitaire.py
+++ tests/test_unitaire.py
@@ -2,13 +2,21 @@ import unittest
 from app.main import main
 import io
 import sys
+from app.main import get_random_message
 
 class TestMain(unittest.TestCase):
-    def test_output(self):
+    def test_hey_output(self):
         original_stdout = sys.stdout
         sys.stdout = io.StringIO()  
         main() 
         output = sys.stdout.getvalue()
-        sys.stdout = original_stdout
-        self.assertEqual(output.strip(), "Hey you!")
+        output = sys.stdout.getvalue().strip().split('\n')[0]
+        self.assertEqual(output, "Hey you!")
+
+    def test_random_message_output(self):
+        messages = ["Keep it up :)", "You got this :)", "You're almost at the finish line :)", "It will be okay :)"]
+        result = get_random_message()
+        self.assertIn(result, messages)
+
+
 
