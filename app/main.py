13584dc11df825a5cd280c4dfc972f96b6f8e599
Aicha Aanounou <aicha.aanounou.1@ens.etsmtl.ca>
2025-05-16 19:15:39 +0000

    Fix pylint issues in main.py: docstrings, formatting, imports

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
+"""Main script that prints messages de motivations."""
+
 import random
+import io
+import sys
+
 
 def get_random_message():
-    message= ["Keep it up :)", "You got this :)", "You're almost at the finish line :)", "It will be okay :)"]
+    """Return a random motivational message."""
+    message = [
+        "Keep it up :)",
+        "You got this :)",
+        "You're almost at the finish line :)",
+        "It will be okay :)"
+    ]
     return random.choice(message)
 
+
 def main():
+    """Print greeting and a random motivational message."""
     print("Hey you!")
     print(get_random_message())
 
+
 if __name__ == "__main__":
     main()
+
