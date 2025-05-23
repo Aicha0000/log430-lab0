459ab8cf2abd8881de3dd295d4736972840f09fe
Aicha Aanounou <aicha.aanounou.1@ens.etsmtl.ca>
2025-05-23 00:48:36 +0000

    Correction des erreurs Pylint

--- app/console.py
+++ app/console.py
@@ -1,42 +1,46 @@
 """
-Fichier console qui est l'interface console du système de caisse
+Fichier console qui est l'interface console du système de caisse.
 """
 
 from app.models import Produit
 from app.persistance.db import session
 
-def afficherConsoleMenu():
+def afficher_console_menu():
+    """Affiche le menu principal de l'application."""
     print("Bienvenue dans le Menu Caisse")
     print("1. Ajouter un produit")
     print("2. Afficher les produits")
     print("3. Supprimer un produit")
     print("4. Quitter")
 
-def ajouterProduit():
+def ajouter_produit():
+    """Demande les infos d’un produit et l’ajoute à la base."""
     nom = input("Entrez le nom du produit : ")
     prix = float(input("Prix: "))
-    description = int(input("Description: "))
-    
+    description = input("Description: ") 
+
     produit = Produit(nom=nom, prix=prix, description=description)
     session.add(produit)
     session.commit()
-    print(f"Produit {nom} ajouté.")
+    print(f"Produit '{nom}' ajouté.")
 
-def afficherProduits():
+def afficher_produits():
+    """Affiche tous les produits en base de données."""
     produits = session.query(Produit).all()
     for produit in produits:
         print(f"ID: {produit.id}, Nom: {produit.nom}, "
               f"Prix: {produit.prix}, Description: {produit.description}")
-              
+
 def main():
+    """Point d'entrée principal de l’application console."""
     while True:
-        afficherConsoleMenu()
+        afficher_console_menu()
         choix = input("Choisissez une option : ")
-        
+
         if choix == '1':
-            ajouterProduit()
+            ajouter_produit()
         elif choix == '2':
-            afficherProduits()
+            afficher_produits()
         elif choix == '3':
             break
         else:
@@ -44,4 +48,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
